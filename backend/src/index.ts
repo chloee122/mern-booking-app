ff2c69b8fcebfa7d3ab61fd674ab6a23d9956717
Cinnamon Bun <nguyeney02@gmail.com>
2025-02-21 15:01:31 +0200

    Handle client-side routing in Express by returning index.html

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,40 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-import path from "path"
-import { v2 as cloudinary } from "cloudinary"
+import path from "path";
+import { v2 as cloudinary } from "cloudinary";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
-import hotelRoutes from "./routes/my-hotels"
+import hotelRoutes from "./routes/my-hotels";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to MongoDB database"));
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to MongoDB database"));
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/my-hotels", hotelRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", hotelRoutes);
+
+app.get("*", function (req: Request, res: Response) {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(3009, () => {
-    console.log("server running on localhost:3009");
-})
+  console.log("server running on localhost:3009");
+});
